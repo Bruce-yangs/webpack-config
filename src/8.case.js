00c40f72ea27a05f52ffb2f7dab0facc22410c53
yangkun <yangkun@leying365.com>
2020-09-15 18:31:15 +0800

    AsyncSeriesWaterfallHook的实现

--- src/8.case.js
+++ src/8.case.js
@@ -5,42 +5,41 @@ class AsyncSeriesWaterfallHook { // 钩子同步方法
     tapAsync(name, task) {
         this.tasks.push(task);
     }
-    callAsync(...args) { 
-       let index = 0;
-       let finalCallBack = args.pop();
-       let next = (err,data) => {
-           let task = this.tasks[index];
-           if (!task) return finalCallBack();
-           if(index === 0) {// 执行的是第一个
-                task(...args,next);
-           } else {
-            task(data,next);
-           }
-           index++;
-       }
-       next();
+    callAsync(...args) {
+        let index = 0;
+        let finalCallBack = args.pop();
+        let next = (err, data) => {
+            let task = this.tasks[index];
+            if (!task) return finalCallBack();
+            if (index === 0) { // 执行的是第一个
+                task(...args, next);
+            } else {
+                task(data, next);
+            }
+            index++;
+        }
+        next();
     }
 }
 let hook = new AsyncSeriesWaterfallHook(['name']);
 hook.tapAsync('vue', function (name, cb) {
-        setTimeout(() => {
-            console.log('vue', name);
-            cb(null,'结果');
-        }, 1000);
-    
+    setTimeout(() => {
+        console.log('vue', name);
+        cb(null, '结果');
+    }, 1000);
 });
 hook.tapAsync('node', function (name, cb) {
-        setTimeout(() => {
-            console.log('node', name);
-            cb(null,'结果1');
-        }, 1000);
+    setTimeout(() => {
+        console.log('node', name);
+        cb(null, '结果1');
+    }, 1000);
 });
 hook.tapAsync('react', function (name, cb) {
-        setTimeout(() => {
-            console.log('react', name);
-            cb(null,'结果2');
+    setTimeout(() => {
+        console.log('react', name);
+        cb(null, '结果2');
     })
 });
-hook.callAsync('Bruce',() => {
+hook.callAsync('Bruce', () => {
     console.log('结束');
 })
